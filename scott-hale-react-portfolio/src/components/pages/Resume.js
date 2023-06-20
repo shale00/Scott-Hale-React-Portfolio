e8dabef5189d2cf6d3e73e72597aeefd57b27aee
sth <sth0067@gmail.com>
2023-06-20 05:17:05 -0500

    added files

---
+++ scott-hale-react-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+      I am Certified Public Accountant licensed in Texas, I compete in annual barbershop quartet competitions, and I love sports!
+      </p>
+    </div>
+  );
+}
