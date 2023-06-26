c6158d9ff7882472d8c1ac38f32ba459b7c65794
sth <sth0067@gmail.com>
2023-06-26 14:14:38 -0500

    added icons

---
+++ scott-hale-react-portfolio/src/components/Icons.js
@@ -0,0 +1,112 @@
+import htmlIcon from './Icons/icons8-html5-color-96.png';
+import cssIcon from './Icons/icons8-css3-96.png';
+import javaScriptIcon from './Icons/icons8-javascript-96.png'
+import mysqlIcon from './Icons/icons8-mysql-96.png'
+import mongoDbIcon from './Icons/icons8-mongodb-96.png'
+import mongooseIcon from './Icons/icons8-mongoose-96.png'
+import gitIcon from './Icons/icons8-git-96.png'
+import gitHubIcon from './Icons/icons8-github-100.png'
+import gitLabIcon from './Icons/icons8-gitlab-96.png'
+import vsCodeIcon from './Icons/icons8-vs-code-96.png'
+import reactIcon from './Icons/logo192.png'
+import tailwindIcon from './Icons/icons8-tailwind-css-96.png'
+import bootstrapIcon from './Icons/icons8-bootstrap-96.png'
+import jqueryIcon from './Icons/icons8-jquery-is-a-javascript-library-designed-to-simplify-html-96.png'
+import restApiIcon from './Icons/icons8-rest-api-96.png'
+import expressJsIcon from './Icons/icons8-express-js-96.png'
+import herokuIcon from './Icons/icons8-heroku-96.png'
+import apiIcon from './Icons/icons8-api-96.png'
+import npmIcon from './Icons/icons8-npm-96.png'
+import jestIcon from './Icons/icons8-jest-can-collect-code-coverage-information-from-entire-projects-96.png'
+import nodeIcon from './Icons/icons8-nodejs-96.png'
+
+
+
+const icons = [
+    {
+        img: htmlIcon,
+        alt: "HTML"
+    },
+    {
+        img: cssIcon,
+        alt: "CSS"
+    },
+    {
+        img: javaScriptIcon,
+        alt: "JavaScript"
+    },
+    {
+        img: reactIcon,
+        alt: "React"
+    },
+    {
+        img: mysqlIcon,
+        alt: "MySql"
+    },
+    {
+        img: mongoDbIcon,
+        alt: "mongoDB"
+    },
+    {
+        img: mongooseIcon,
+        alt: "Mongoose"
+    },
+    {
+        img: gitIcon,
+        alt: "Git"
+    },
+    {
+        img: gitHubIcon,
+        alt: "GitHub"
+    },
+    {
+        img: gitLabIcon,
+        alt: "GitLab"
+    },
+    {
+        img: vsCodeIcon,
+        alt: "VSCode"
+    },
+    {
+        img: jqueryIcon,
+        alt: "JQuery"
+    },
+    {
+        img: bootstrapIcon,
+        alt: "Bootstrap"
+    },
+    {
+        img: tailwindIcon,
+        alt: "Tailwind CSS"
+    },
+    {
+        img: herokuIcon,
+        alt: "Heroku"
+    },
+    {
+        img: npmIcon,
+        alt: "NPM"
+    },
+    {
+        img: nodeIcon,
+        alt: "VSCode"
+    },
+    {
+        img: apiIcon,
+        alt: "API"
+    },
+    {
+        img: restApiIcon,
+        alt: "Rest API"
+    },
+    {
+        img: expressJsIcon,
+        alt: "Express.js"
+    },
+    {
+        img: jestIcon,
+        alt: "Jest"
+    },
+];
+
+export default icons;
--- scott-hale-react-portfolio/src/components/pages/Resume.js
+++ scott-hale-react-portfolio/src/components/pages/Resume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import icons from "../Icons";
 
 export default function Resume() {
   return (
@@ -9,6 +10,17 @@ export default function Resume() {
         <li>I've engineered full-stack web applications, working with servers, databases, and other back end technologies, and connecting them to the front end.</li>
         <li>Most recently I've learned how to create single-page applications with React (this portfolio!).</li>
       </ul>
+
+      <div>
+        
+        {icons.map((icon, i) => (
+          <img key={i} src={icon.img} alt={icon.alt} className="object-scale-down h-14" />
+        ))}
+
+        
+      </div>
+      <a target="_blank" href="https://icons8.com/icon/21278/css3"></a> Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+
     </div>
   );
 }
